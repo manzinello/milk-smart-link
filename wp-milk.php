e7a8d707128523c5ed9824c1797e40baace6986e
Matteo Manzinello <matteo@manzinello.com>
2019-03-04 14:26:40 +0100

    init

---
+++ wp-milk.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Milk-smartlink plugin for WordPress
+ *
+ * @link              https://github.com/manzinello/wp-milk
+ * @since             0.1.0
+ * @package           milk
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Milk
+ * Plugin URI:        https://github.com/manzinello/wp-milk
+ * Description:       Milk smart link redirection for WordPress
+ * Version:           0.1.0
+ * Author:            Matteo Manzinello
+ * Author URI:        https://matteomanzinello.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
