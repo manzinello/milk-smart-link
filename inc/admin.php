2dc68856b9c12dcafa509b5427f79c0bb768468c
Matteo Manzinello <matteo@manzinello.com>
2019-03-05 11:16:06 +0100

    Bozza html admin page

--- inc/admin.php
+++ inc/admin.php
@@ -12,6 +12,16 @@ function milk_settings_page()
     ob_start();
     ?>
 
+    <form action="/action_page.php">
+        First name:<br>
+        <input type="text" name="firstname" value="Mickey">
+        <br>
+        Last name:<br>
+        <input type="text" name="lastname" value="Mouse">
+        <br><br>
+        <input type="submit" value="Submit">
+    </form>
+
     <?php
 
     return ob_get_clean();
