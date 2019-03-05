ad9e929b9a8d87ced9b8c69e0266219baa6a1570
Matteo Manzinello <matteo@manzinello.com>
2019-03-05 11:02:34 +0100

    Pulizia del codice

--- inc/os.php
+++ inc/os.php
@@ -68,14 +68,3 @@ function getBrowser()
 
     return $browser;
 }
-
-/*
-$user_os        = getOS();
-$user_browser   = getBrowser();
-
-$device_details = "<strong>Browser: </strong>" . $user_browser . "<br /><strong>Operating System: </strong>" . $user_os . "";
-
-print_r($device_details);
-
-echo ("<br /><br /><br />" . $_SERVER['HTTP_USER_AGENT'] . "");
-*/
